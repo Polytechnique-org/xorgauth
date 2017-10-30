6264d464407c4478f92286e9b71a84427f733ac9
Nicolas Iooss <nicolas.iooss_git@polytechnique.org>
2017-10-30 23:22:28 +0100

    Add example templates
    Copy templates from the documentation, in order to test the backend

--- xorgauth/urls.py
+++ xorgauth/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^openid/', include('oidc_provider.urls', namespace='oidc_provider')),
+    url(r'^accounts/login/$', auth_views.LoginView.as_view(), name='login'),
+    url(r'^accounts/logout/$', auth_views.LogoutView.as_view(), name='logout'),
 ]
