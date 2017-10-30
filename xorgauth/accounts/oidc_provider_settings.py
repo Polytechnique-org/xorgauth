4be9346a80e94a5c5794fc08284e4e59d98f3ec0
Nicolas Iooss <nicolas.iooss_git@polytechnique.org>
2017-10-30 23:36:31 +0100

    Fix tox lint

--- xorgauth/accounts/oidc_provider_settings.py
+++ xorgauth/accounts/oidc_provider_settings.py
@@ -2,6 +2,7 @@
 # Copyright (c) Polytechnique.org
 # This code is distributed under the Affero General Public License version 3
 
+
 def userinfo(claims, user):
     """Populate claims dict for the given user
 
