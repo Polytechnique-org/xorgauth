962fc5e2295629ca91b409efd60c8941b2a4a36b
Victor Berger <victor.berger@m4x.org>
2017-11-11 17:25:23 +0100

    Order oidc_provider after xorgauth in settings
    Otherwise, oidc_provider does not use our custom templates.

--- xorgauth/settings.py
+++ xorgauth/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.auth',
-    'oidc_provider',
     'xorgauth',
+    'oidc_provider',
 ]
 
 MIDDLEWARE = [
