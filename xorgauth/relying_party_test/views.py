aabf2cb6cb26f053fd26dbb92cebc774f6f970fe
Nicolas Iooss <nicolas.iooss_git@polytechnique.org>
2017-11-19 22:35:08 +0100

    Show development accounts in RP test page
    This eases to remember which accounts exist

--- xorgauth/relying_party_test/views.py
+++ xorgauth/relying_party_test/views.py
@@ -1,5 +1,18 @@
+import json
+import os.path
+
+from django.conf import settings
 from django.views.generic import TemplateView
 
 
 class RelyingParty(TemplateView):
     template_name = 'relying-party-test.html'
+
+    def get_context_data(self, **kwargs):
+        kwargs = super(RelyingParty, self).get_context_data(**kwargs)
+        # Show some useful accounts in development mode
+        if settings.DEBUG:
+            with open(os.path.join(settings.BASE_DIR, 'scripts', 'dev_data.json')) as fd:
+                json_data = json.load(fd)
+                kwargs['dev_accounts'] = json_data['accounts']
+        return kwargs
