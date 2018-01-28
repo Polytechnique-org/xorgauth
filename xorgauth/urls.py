be01d4fb27af6e6feebc4e5b69fec317e9df8391
Nicolas Iooss <nicolas.iooss_git@polytechnique.org>
2018-01-28 17:08:54 +0100

    Wire up a password reset form which accepts hrids too
    NB: The password recovery email is always sent to the main email
    address, even when an email alias has been submitted.

--- xorgauth/urls.py
+++ xorgauth/urls.py
@@ -30,7 +30,7 @@ urlpatterns = [
     url(r'^accounts/list_consents/$', xorgauth_views.list_consents, name='list_consents'),
     url(r'^accounts/password/change/$', xorgauth_views.PasswordChangeView.as_view(), name='password_change'),
     url(r'^accounts/password/change/done/$', auth_views.password_change_done, name='password_change_done'),
-    url(r'^accounts/password/reset/$', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    url(r'^accounts/password/reset/$', xorgauth_views.PasswordResetView.as_view(), name='password_reset'),
     url(r'^accounts/password/reset/done/$', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     url(r'^accounts/password/reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',  # noqa
         auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
