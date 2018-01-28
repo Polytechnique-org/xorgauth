7f93783b04f49924896d98f9550ef95bc37d0a77
Nicolas Iooss <nicolas.iooss_git@polytechnique.org>
2018-01-28 16:42:48 +0100

    Move the identification logic of the login system to the User Manager
    As the password recovery system needs to accept both emails and email
    aliases, factor out the code into models.py.

--- xorgauth/accounts/authentication.py
+++ xorgauth/accounts/authentication.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.backends import ModelBackend
 
-from xorgauth.accounts.models import User, UserAlias
+from xorgauth.accounts.models import User
 
 
 class XorgBackend(ModelBackend):
@@ -10,40 +10,10 @@ class XorgBackend(ModelBackend):
     just overriding the user search given an hrid or email
     """
     def authenticate(self, request, username=None, password=None, **kwargs):
-        if '@' not in username:
-            # try to login with hrid
-            try:
-                user = User.objects.get(hrid=username)
-            except User.DoesNotExist:
-                # if the username is "firstname.lastname", try builing an hrid
-                user = None
-                hrid_prefix = username + '.'
-                for possible_user in User.objects.filter(hrid__startswith=hrid_prefix):
-                    # Sanity check
-                    if not possible_user.hrid.startswith(hrid_prefix):
-                        return None
-                    # No dot in the last part
-                    if '.' in possible_user.hrid[len(hrid_prefix):]:
-                        continue
-                    # Several users share the same firstname.lastname prefix,
-                    # refuse to log in.
-                    if user is not None:
-                        return None
-                    user = possible_user
-                # Return nothing if no user has been found
-                if user is None:
-                    return None
-        else:
-            # try to login with email
-            try:
-                # try main email
-                user = User.objects.get(main_email=username)
-            except User.DoesNotExist:
-                # try aliases
-                try:
-                    user = UserAlias.objects.get(email=username).user
-                except UserAlias.DoesNotExist:
-                    return None
+        user = User.objects.get_for_login(username)
+        if user is None:
+            return
+
         # we now have a candidate user
         if user.check_password(password) and self.user_can_authenticate(user):
             return user
--- xorgauth/accounts/models.py
+++ xorgauth/accounts/models.py
@@ -49,6 +49,50 @@ class UserManager(base_user.BaseUserManager):
         user.save(using=self._db)
         return user
 
+    def get_for_login(self, username):
+        """Get a user for the given username, mail email or email alias
+
+        For example the given username can come from a login form in order to
+        identify a user.
+
+        Returns None if no user has been found
+        """
+        if '@' not in username:
+            # try to login with hrid (human-readable identifier)
+            try:
+                return self.get(hrid=username)
+            except User.DoesNotExist:
+                pass
+
+            # if the username is "firstname.lastname", try builing an hrid
+            user = None
+            hrid_prefix = username + '.'
+            for possible_user in self.filter(hrid__startswith=hrid_prefix):
+                # Sanity check
+                if not possible_user.hrid.startswith(hrid_prefix):
+                    return None
+                # No dot in the last part
+                if '.' in possible_user.hrid[len(hrid_prefix):]:
+                    continue
+                # Several users share the same firstname.lastname prefix,
+                # refuse to log in.
+                if user is not None:
+                    return None
+                user = possible_user
+            # Return nothing if no user has been found, and the user otherwise
+            return user
+        else:
+            # try to login with email
+            try:
+                # try main email
+                return self.get(main_email=username)
+            except User.DoesNotExist:
+                # try aliases
+                try:
+                    return UserAlias.objects.get(email=username).user
+                except UserAlias.DoesNotExist:
+                    return None
+
 
 class User(base_user.AbstractBaseUser):
     MALE = 'male'
