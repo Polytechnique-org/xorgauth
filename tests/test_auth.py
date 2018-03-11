Polytechnique-org/xorgauth

commit ec35e81065752485c85e013d3a1959545e7e3577

Save a hashed password for Google Apps

tests/test_auth.py
@@ -1,3 +1,5 @@
+import crypt
+
 from django.test import Client, TestCase
 
 from xorgauth.accounts.models import User, UserAlias
@@ -64,3 +66,16 @@ class AuthenticationTests(TestCase):
         self.assertEqual(None, User.objects.get_for_login('louis.vaneau.1830@polytechnique.org', True))
         c = Client()
         self.assertFalse(c.login(username='louis.vaneau.1830@polytechnique.org', password='Depuis Vaneau!'))
+
+    def test_googleapps_password(self):
+        """Test setting the Google Apps password when setting the password"""
+        self.vaneau.set_password('Depuis Vaneau!')
+        self.vaneau.save()
+        gapps_password = self.vaneau.gapps_password.password
+        self.assertEqual(crypt.crypt('Depuis Vaneau!', gapps_password), gapps_password)
+
+        self.vaneau.set_password('Mot de passe différent?')
+        self.vaneau.save()
+        self.vaneau = User.objects.get(hrid='louis.vaneau.1829')
+        gapps_password = self.vaneau.gapps_password.password
+        self.assertEqual(crypt.crypt('Mot de passe différent?', gapps_password), gapps_password)

xorgauth/accounts/password_validators.py
@@ -1,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) Polytechnique.org
+# This code is distributed under the Affero General Public License version 3
+import crypt
+
+from django.core.exceptions import ObjectDoesNotExist
+
+from . import models
+
+
+class GoogleAppsPasswordValidator(object):
+    """Update the Google Apps password when a user changes her password"""
+
+    def password_changed(self, raw_password, user):
+        # Hash the password in a way compatible with Google Apps: crypt with $6
+        password = crypt.crypt(raw_password, salt=crypt.METHOD_SHA512)
+        try:
+            user.gapps_password.password = password
+        except ObjectDoesNotExist:
+            models.GoogleAppsPassword.objects.create(user=user, password=password)
+        else:
+            user.gapps_password.save()

xorgauth/settings.py
@@ -170,6 +170,14 @@ PASSWORD_HASHERS = [
     'xorgauth.accounts.hashers.PBKDF2WrappedSHA1PasswordHasher',
 ]
 
+# Password validators
+# https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'xorgauth.accounts.password_validators.GoogleAppsPasswordValidator',
+    }
+]
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/

commit a27bd5d4f456246a2197bc56ff3b0d1a733c9971

Add tests for SetPasswordForm and PasswordChangeForm
These forms have been broken since commit ec35e8106575 ("Save a hashed
password for Google Apps") and fixed by commit 4d1a7b48b450 ("Add
missing validate method to GoogleAppsPasswordValidator"). In order to
prevent a future regression, use these forms in tests.

The new tests would have detected the bug by failing with the following
exception:

    AttributeError: 'GoogleAppsPasswordValidator' object has no
    attribute 'validate'

Closes: https://github.com/Polytechnique-org/xorgauth/issues/79

tests/test_auth.py
@@ -174,6 +174,55 @@ class AuthenticationTests(TestCase):
         }, form.errors)
         self.assertEqual({}, form.cleaned_data)
 
+    def test_password_set_form(self):
+        """Test using the password set form"""
+        c = Client()
+        self.assertTrue(c.login(username='louis.vaneau.1829', password='Depuis Vaneau!'))
+        form = xorgauth.forms.SetPasswordForm(user=self.vaneau, data={
+            'new_password1': 'Mot de passe différent?',
+            'new_password2': 'Mot de passe différent?',
+        })
+        self.assertTrue(form.is_valid())
+        self.assertEqual('Mot de passe différent?', form.cleaned_data['new_password1'])
+        form.save()
+        # Athentication with the previous password fails and it works with the new password
+        self.assertFalse(c.login(username='louis.vaneau.1829', password='Depuis Vaneau!'))
+        self.assertTrue(c.login(username='louis.vaneau.1829', password='Mot de passe différent?'))
+
+    def test_password_change_form(self):
+        """Test using the password change form"""
+        c = Client()
+        self.assertTrue(c.login(username='louis.vaneau.1829', password='Depuis Vaneau!'))
+        form = xorgauth.forms.PasswordChangeForm(user=self.vaneau, data={
+            'old_password': 'Depuis Vaneau!',
+            'new_password1': 'Mot de passe différent?',
+            'new_password2': 'Mot de passe différent?',
+        })
+        self.assertTrue(form.is_valid())
+        self.assertEqual('Mot de passe différent?', form.cleaned_data['new_password1'])
+        form.save()
+        # Athentication with the previous password fails and it works with the new password
+        self.assertFalse(c.login(username='louis.vaneau.1829', password='Depuis Vaneau!'))
+        self.assertTrue(c.login(username='louis.vaneau.1829', password='Mot de passe différent?'))
+
+    def test_password_change_form_incorrect_oldpass(self):
+        """Test using the password change form with an incorrect old password"""
+        c = Client()
+        self.assertTrue(c.login(username='louis.vaneau.1829', password='Depuis Vaneau!'))
+        form = xorgauth.forms.PasswordChangeForm(user=self.vaneau, data={
+            'old_password': 'Depuis Vaneau?',
+            'new_password1': 'Mot de passe différent?',
+            'new_password2': 'Mot de passe différent?',
+        })
+        self.assertFalse(form.is_valid())
+        self.assertEqual({
+            'old_password': ['Votre ancien mot de passe est incorrect. Veuillez le rectifier.'],
+        }, form.errors)
+        self.assertEqual({
+            'new_password1': 'Mot de passe différent?',
+            'new_password2': 'Mot de passe différent?',
+        }, form.cleaned_data)
+
     def test_auth_session_expiry(self):
         c = Client()
         c.post('/accounts/login/', {'username': 'louis.vaneau.1829', 'password': 'Depuis Vaneau!', 'expiry': 'now'})