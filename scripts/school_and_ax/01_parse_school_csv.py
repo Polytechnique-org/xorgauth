147b6003ca953a5dc7a986aa67a2f31a5b0c0bfe
Nicolas Iooss <nicolas.iooss_git@polytechnique.org>
2019-01-21 22:25:23 +0100

    Make 01_parse_school_csv.py accept the school Excel directly
    This avoids the need of converting the Excel downloaded from the school
    website to CSV.

--- scripts/school_and_ax/01_parse_school_csv.py
+++ scripts/school_and_ax/01_parse_school_csv.py
@@ -24,6 +24,8 @@ import os.path
 import re
 import sys
 
+from openpyxl import load_workbook, Workbook
+
 
 def fix_name_case(name):
     """Fix the case of some names in order to match AX file"""
@@ -63,8 +65,8 @@ def parse_ax_file(filepath):
 
 def main():
     parser = argparse.ArgumentParser(description="Parse a CSV file and output lists for Polytechnique.org website")
-    parser.add_argument('csvfile', nargs=1,
-                        help="CSV file to import")
+    parser.add_argument('school_excel_file', type=str,
+                        help="Excel file produced by the Ecole polytechnique (Guichet_ORG_DD_MM_YYYY.xlsx)")
     parser.add_argument('-a', '--axfile', type=str,
                         help="CSV file with AX IDs")
     parser.add_argument('-o', '--outdir', type=str,
@@ -78,62 +80,81 @@ def main():
     data_by_promo = {}
 
     header_row = None
-    with open(args.csvfile[0], 'r') as fcsv:
-        reader = csv.reader(fcsv, delimiter=',', quotechar='"', escapechar='\\', strict=True)
-        for row in reader:
-            if header_row is None:
-                header_row = row
+    school_data = []
+    if args.school_excel_file.lower().endswith('.csv'):
+        # Parse a CSV file from School's Excel file
+        with open(args.school_excel_file, 'r') as fcsv:
+            reader = csv.reader(fcsv, delimiter=',', quotechar='"', escapechar='\\', strict=True)
+            for row in reader:
+                if header_row is None:
+                    header_row = row
+                    continue
+                assert len(header_row) == len(row), "CSV file is ill-formed"""
+                row_fields = dict(zip(header_row, row))
+                school_data.append(row_fields)
+    elif args.school_excel_file.lower().endswith('.xlsx'):
+        # Parse School's Excel file
+        school_wb = load_workbook(filename=args.school_excel_file, read_only=True)
+        school_ws = school_wb.worksheets[0]
+        for school_row in school_ws.iter_rows():
+            if school_row[0].row == 1:
+                header_row = [cell.value for cell in school_row]
                 continue
-            assert len(header_row) == len(row), "CSV file is ill-formed"""
-            row_fields = dict(zip(header_row, row))
+            assert len(header_row) == len(school_row), "Excel file is ill-formed"""
+            row_fields = dict((name, str(cell.value)) for (name, cell) in zip(header_row, school_row))
+            school_data.append(row_fields)
+    else:
+        parser.error("Unsupported file extension of school CSV or Excel file ({})".format(
+            repr(args.school_excel_file)))
 
-            first_name = fix_name_case(row_fields['prénom'])
-            last_name = fix_name_case(row_fields['nom'])
-            birthdate = row_fields['date de naissance']
-            sex = {'M': 'M', 'MME': 'F'}[row_fields['civilité']]
-            school_id = row_fields['matricule']
-            promo_kind = row_fields['type']
-            promo_year = row_fields['promotion']
+    for school_fields in school_data:
+        first_name = fix_name_case(school_fields['prénom'])
+        last_name = fix_name_case(school_fields['nom'])
+        birthdate = school_fields['date de naissance']
+        sex = {'M': 'M', 'MME': 'F'}[school_fields['civilité']]
+        school_id = school_fields['matricule']
+        promo_kind = school_fields['type']
+        promo_year = school_fields['promotion']
 
-            # Reject people who did not agree to share their data
-            if not birthdate:
-                continue
+        # Reject people who did not agree to share their data
+        if not birthdate:
+            continue
 
-            # Graduate degrees have promotion = entry year
-            if promo_kind in ('GD', 'BC'):
-                promo_year = row_fields['année admission']
+        # Graduate degrees have promotion = entry year
+        if promo_kind in ('GD', 'BC'):
+            promo_year = school_fields['année admission']
 
-            # Get the AX ID
-            ax_id = ''
-            ax_key = (last_name, first_name)
-            if ax_key in ax_data:
-                row_ax_data = ax_data[ax_key]
-                if row_ax_data['birthdate'] and row_ax_data['birthdate'] != birthdate:
-                    sys.stderr.write("Error: mismatched birthdate for {}: {} vs {}\n".format(
-                        repr(ax_key),
-                        row_ax_data['birthdate'],
-                        birthdate,
-                    ))
-                else:
-                    ax_id = row_ax_data['ax_id']
-            elif ax_data:
-                sys.stderr.write("Warning: {} not in AX data ({} {})\n".format(
-                    repr(ax_key), promo_kind, promo_year))
+        # Get the AX ID
+        ax_id = ''
+        ax_key = (last_name, first_name)
+        if ax_key in ax_data:
+            row_ax_data = ax_data[ax_key]
+            if row_ax_data['birthdate'] and row_ax_data['birthdate'] != birthdate:
+                sys.stderr.write("Error: mismatched birthdate for {}: {} vs {}\n".format(
+                    repr(ax_key),
+                    row_ax_data['birthdate'],
+                    birthdate,
+                ))
+            else:
+                ax_id = row_ax_data['ax_id']
+        elif ax_data:
+            sys.stderr.write("Warning: {} not in AX data ({} {})\n".format(
+                repr(ax_key), promo_kind, promo_year))
 
-            # Table header:
-            #   Nom Prénom Date de naissance (JJ/MM/AAAA) Sexe (F/M) Matricule École Matricule AX
-            xorg_fields = (
-                last_name,
-                first_name,
-                birthdate,
-                sex,
-                school_id,
-                ax_id,
-            )
-            promo = promo_kind + ' ' + promo_year
-            if promo not in data_by_promo:
-                data_by_promo[promo] = []
-            data_by_promo[promo].append(xorg_fields)
+        # Table header:
+        #   Nom Prénom Date de naissance (JJ/MM/AAAA) Sexe (F/M) Matricule École Matricule AX
+        xorg_fields = (
+            last_name,
+            first_name,
+            birthdate,
+            sex,
+            school_id,
+            ax_id,
+        )
+        promo = promo_kind + ' ' + promo_year
+        if promo not in data_by_promo:
+            data_by_promo[promo] = []
+        data_by_promo[promo].append(xorg_fields)
 
     if args.outdir:
         if not os.path.exists(args.outdir):
