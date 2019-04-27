5f79d8e7bb631a08d7d35e93c6633c0e3f045d17
Nicolas Iooss <nicolas.iooss_git@polytechnique.org>
2019-04-27 16:44:41 +0200

    Use PasswordChangeDoneView instead of password_change_done
    Django warns about a deprecated view:
    
        xorgauth/.tox/py37-django22/lib/python3.7/site-packages/django/contrib/auth/decorators.py:21:
        RemovedInDjango21Warning: The password_change_done() view is
        superseded by the class-based PasswordChangeDoneView().

--- xorgauth/urls.py
+++ xorgauth/urls.py
@@ -23,6 +23,13 @@ from xorgauth.authgroupex import views as authgpx_views
 from xorgauth.relying_party_test import views as rptest_views
 from xorgauth import forms as xorgauth_forms
 
+
+if hasattr(auth_views, 'PasswordChangeDoneView'):
+    # Django 2.1 replaced auth_password_change_done_view by PasswordChangeDoneView
+    auth_password_change_done_view = auth_views.PasswordChangeDoneView.as_view()
+else:
+    auth_password_change_done_view = auth_views.password_change_done
+
 urlpatterns = [
     url(r'^$', xorgauth_views.IndexView.as_view(), name='index'),
     url(r'^admin/', admin.site.urls),
@@ -34,7 +41,7 @@ urlpatterns = [
     url(r'^accounts/logout/$', auth_views.LogoutView.as_view(), name='logout'),
     url(r'^accounts/list_consents/$', xorgauth_views.list_consents, name='list_consents'),
     url(r'^accounts/password/change/$', xorgauth_views.PasswordChangeView.as_view(), name='password_change'),
-    url(r'^accounts/password/change/done/$', auth_views.password_change_done, name='password_change_done'),
+    url(r'^accounts/password/change/done/$', auth_password_change_done_view, name='password_change_done'),
     url(r'^accounts/password/reset/$', xorgauth_views.PasswordResetView.as_view(), name='password_reset'),
     url(r'^accounts/password/reset/done/$', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     url(r'^accounts/password/reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',  # noqa
