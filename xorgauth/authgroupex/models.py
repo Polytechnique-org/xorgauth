22c29155e671b46d64197cdddaa3294e47e3df71
pierre-m-magnan <pier.magnan@gmail.com>
2025-05-26 18:34:26 +0200

    updates Django to 5.2.1 + django-bootstrap3->5

--- xorgauth/authgroupex/models.py
+++ xorgauth/authgroupex/models.py
@@ -8,7 +8,7 @@ import sys
 
 from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from xorgauth.utils.fields import UnboundedCharField
 
 
--- xorgauth/utils/fields.py
+++ xorgauth/utils/fields.py
@@ -6,7 +6,7 @@ import re
 from django.core.validators import RegexValidator
 from django.db import models
 from django import forms
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 class UnboundedCharField(models.TextField):
