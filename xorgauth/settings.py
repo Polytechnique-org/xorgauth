4d4bce3650a2290494131547756bf6ccc356051a
PositroniumJS <150566116+PositroniumJS@users.noreply.github.com>
2025-09-21 23:12:55 +0200

    Remove useless import in python3
    `from __future__ import unicode_literals` was needed with python2

--- xorgauth/settings.py
+++ xorgauth/settings.py
@@ -10,8 +10,6 @@ https://docs.djangoproject.com/en/1.11/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
-from __future__ import unicode_literals
-
 import getconf
 import os
 
